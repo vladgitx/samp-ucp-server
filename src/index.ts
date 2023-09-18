e0f7e0fb958e26a7f8879ca4cf9bd86ca7c14892
vladbalea <vladd.balea@gmail.com>
2023-09-18 19:50:53 +0300

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express"
+import cors from "cors"
+
 import { router as userRouter } from "./routes/applications"
 
 const app = express()
-const PORT = 3000
+const PORT = 3001
 
+app.use(cors({ origin: "http://localhost:3000" }))
 app.use(express.json())
 app.use("/applications", userRouter)
 
